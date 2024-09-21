0f48b437b42ee01be0727b3edff514b92bea1fa4
Ivan Antonov <ivan.antonov.pro@gmail.com>
2024-09-22 00:04:02 +0500

    instantly report

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,6 +1,6 @@
 export function getFormatReportTitle(input: string): string {
   if (input.includes('|')) {
-    const name = input.split('|')[0].trim(); 
+    const name = input.split('|')[0].trim() + "_";; 
     return '#' + name.replace(/\s+/g, '_') + '10X';
   } else {
     return input.trim(); 
